14471c10e54c08f8ce196d861e9246d39bc6ab3b
botocgaixinh <83125228+botocgaixinh@users.noreply.github.com>
2021-04-27 15:28:44 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -5,15 +5,16 @@ from telethon import events, Button
 
 @RyoStar.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("SHARE ANH EM VÀI NHÓM NGON 🐰❤❤! \n\n
-                        1️⃣ t.me/joinchat/yzw782riS9s1Njll \n
-                         2️⃣t.me/nhomcuanang",
+    await event.reply("SHARE ANH EM VÀI NHÓM NGON 🐰❤❤! \n\n 1️⃣ t.me/joinchat/yzw782riS9s1Njll \n
+                         2️⃣ t.me/joinchat/p5yntuuOIOoxMDE9 \n
+
+",
                     buttons=[
                         [Button.url("NHÓM CỦA NẮNG ❤", url="https://t.me/nhomcuanang")],
                         [Button.url("KÊNH CỦA NẮNG 🌞",url="https://t.me/kenhcuanang")]
                     ])
 
-@RyoStar.on(events.NewMessage(incoming=True, pattern="/start"))
+@RyoStar.on(events.NewMessage(incoming=True, pattern="start"))
 async def start(event):
     await event.reply("SHARE ANH EM 2 GROUP NGON 🐰❤❤!",
                     buttons=[
