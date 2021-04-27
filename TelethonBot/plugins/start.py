6c0df2178b5050e624153ba13a66d4ad329b5a9c
botocgaixinh <83125228+botocgaixinh@users.noreply.github.com>
2021-04-27 15:53:50 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -5,7 +5,7 @@ from telethon import events, Button
 
 @RyoStar.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("SHARE ANH EM VÀI NHÓM NGON 🐰❤❤!\n\n• t.me/joinchat/yzw782riS9s1Njll \n• t.me/joinchat/p5yntuuOIOoxMDE9 \n• t.me/congdongnang",
+    await event.reply("SHARE ANH EM VÀI NHÓM NGON 🐰❤❤!\n\n• t.me/joinchat/yzw782riS9s1Njll \n\n• t.me/joinchat/p5yntuuOIOoxMDE9 \n\n• t.me/congdongnang",
                     buttons=[
                         [Button.url("NHÓM CỦA NẮNG ❤", url="https://t.me/nhomcuanang"),Button.url("KÊNH CỦA NẮNG 🌞",url="https://t.me/kenhcuanang")],
                         [Button.url("XEM VIDEO HOT TẠI ĐÂY 🔞",url="https://t.me/ditmemay_bot?startgroup=start")]
