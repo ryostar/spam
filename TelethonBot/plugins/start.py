2dfb27a33cccd19baac38cc0b103e92264db38e9
botocgaixinh <83125228+botocgaixinh@users.noreply.github.com>
2021-04-27 20:29:39 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -5,7 +5,7 @@ from telethon import events, Button
 
 @RyoStar.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("👌❤❤😢🤖❤\nTẢI APP XEM LIVE MIỄN PHÍ\n📜❤🤒😭🤨😍🐰❤!\n\n• Tải app [HOTLIVE32.COM](HOTLIVE32.COM)\n• Tải app [MMLIVE](mm636.vip) \n\n• Tải app [QQLIVE](http://WWW.QQL849.COM) \n\n CHỈ CẦN NẠP 20K LÀ VÔ ĐƯỢC PHÒNG VIP BAO PHÊ ❤❤❤🔞🔞",
+    await event.reply("👌❤❤😢🤖❤\nTẢI APP XEM LIVE MIỄN PHÍ\n📜❤🤒😭🤨😍🐰❤!\n\n• Tải app [HOTLIVE32.COM](HOTLIVE32.COM)\n• Tải app [MMLIVE](mm636.vip)\n• Tải app [QQLIVE](http://WWW.QQL849.COM) \n\n CHỈ CẦN NẠP 20K LÀ VÔ ĐƯỢC PHÒNG VIP BAO PHÊ ❤❤❤🔞🔞",
                     buttons=[
                         [Button.url("Tải ngay HOTLIVE32.COM ❤", url="hotlive32.com")],
 [Button.url("Tải ngay MM636.VIP 🔞",url="mm636.vip")],
