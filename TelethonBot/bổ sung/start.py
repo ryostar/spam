a6921a5353aff3de9894cc140ed0ea07c05b1fc2
ryostar <82922436+ryostar@users.noreply.github.com>
2021-04-28 05:33:08 +0700

    Update and rename TelethonBot/plugins/start.py to TelethonBot/bổ sung/start.py

--- TelethonBot/bổ sung/start.py
+++ TelethonBot/bổ sung/start.py
@@ -5,11 +5,11 @@ from telethon import events, Button
 
 @RyoStar.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("EVENT GROUP LIVE FREE\n!❤🤒😭🎀🤨😍🐰❤!\n\nNữ Sinh - IDOL - Đủ Thể Loại - 2K => 2k3 - SOME - MÓC BƯỚM - GIAO LƯU - NHÀ NGHỈ 🤟\n\n♯ Xem hướng dẫn: [BẤM VÀO ĐÂY](http://bit.ly/3dYTXJr)\n\nCó Bán Vé Vip LIVE ZALO 🤘 ༻@Ryo_Star༺",
+    await event.reply("EVENT GROUP LIVE FREE\n!❤🤒😭🎀🤨😍🐰❤!\n\nNữ Sinh - IDOL - Đủ Thể Loại - 2K => 2k3 - SOME - MÓC BƯỚM - GIAO LƯU - NHÀ NGHỈ 🤟\n\n♯ Xem hướng dẫn: [BẤM VÀO ĐÂY](http://t.me/joinchat/HFMeDVI4K_czMjBl)\n\n⚠️LƯU Ý: Điện thoại IOS cần vào => cài đặt chung => quản lý thiết bị=> chọn tin cậy APP\n\nCó Bán Vé Vip LIVE ZALO 🤘 ༻@Ryo_Star༺",
                     buttons=[
                         [Button.url("ıllıllı BẤM NÚT PHÍA DƯỚI ıllıllı", url="http://t.me/hoptacxa0")],
 [Button.url("PHÒNG LIVE MỐC BÍM SỐ 1 🎬 ",url="http://bit.ly/3u34kRW")],
                         [Button.url("PHÒNG LIVE ẸT ẸT SOME SỐ 2 🎬",url="http://bit.ly/3sWInCE")],
 [Button.url("PHÒNG LIVE TÂM SỰ SỐ 3 🎬",url="http://bit.ly/3x2JVhW")],
-[Button.url("SÉT KÈO IDOL CALL UY TÍN🔞",url="http://t.me/joinchat/HFMeDVI4K_czMjBl")]
+[Button.url("300K-10P Giá Call Với IDOL",url="https://zalo.me/setcalluytin")]
                     ])
